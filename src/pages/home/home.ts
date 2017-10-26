f64065dde674a0362a7ae5609eb4f683bce374a6
AREON\Gritsayukser <gsvserg@ukr.net>
2017-10-26 19:25:11 +0300

    2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -7,15 +8,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
 
   }
   doRefresh(refresher){
     //alert("refresh!");
-    setTimeout(() => {
+    /*setTimeout(() => {
       console.log('Async operation has ended');
       refresher.complete();
-    }, 2000);
+    }, 2000);*/
+/////
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    let options = new RequestOptions({ headers: headers });
 
+    let postParams = {
+      id: '6'
+    }
+
+    this.http.post("https://api.crmtour.com/api/joke/read_one.php", postParams, options)
+      .subscribe(data => {
+        alert("data");
+        refresher.complete();
+      }, error => {
+        alert(error);// Error getting the data
+        refresher.complete();
+      });
   }
+/////
+
+
 }
