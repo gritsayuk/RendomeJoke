d11f3a0901263f3f3de68f2a100ea7bf55cd9184
AREON\Gritsayukser <gsvserg@ukr.net>
2017-11-03 18:41:55 +0200

    7

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,23 +7,57 @@ import { RestapiServiceProvider } from '../../providers/restapi-service/restapi-
   templateUrl: 'home.html'
 })
 export class HomePage {
-  apiUrl:string = "https://api.crmtour.com/api/joke/read_one.php";
-  JokeBody: any;
+  //apiUrl:string = "https://api.crmtour.com/api/joke/read_one.php";
+  apiUrl:string = "https://api.crmtour.com/api/joke/read_paging.php?page=";
+  JokeBody: Object[];//Масив шуток
+  JokeNum: number;//Номер шутки
+  JokePage: number;//Номер страницы
+  JokePageLen: number;//Количество всех шуток
+
+  JokeNumId: string;//Ид шутки
+  JokeNumName: string;//Шутка
   constructor(public navCtrl: NavController, public restapiService: RestapiServiceProvider) {
-    this.JokeBody = JSON.parse('{"id":"","text":""}');
-    this.SendRequest({"id":"1"});
+    this.JokeNumId = "";
+    this.JokeNumName = "";
+    this.JokeNum = 0;
+    this.JokePage = 0;
+    this.JokePageLen = 0;
+    this.SendRequest("");
+    //this.JokeBody = JSON.parse('[{"id":"","text":""},{}]');
   }
 
   doRefresh(refresher){
-    this.SendRequest({"id":"6"});
+    this.JokeNum++;
+    if(this.JokeNum>=this.JokePageLen-1){
+      this.SendRequest("");
+    }
+    this.JokeNumId = this.JokeBody[this.JokeNum]["id"];
+    this.JokeNumName = this.JokeBody[this.JokeNum]["name"];
+    //console.log(this.JokeNum);
+    //this.SendRequest("");
     refresher.complete();
   }
   SendRequest(req){
-    this.restapiService.runHttpPost(this.apiUrl,req)
-      .then((result) => {
-        //console.log("result:"+result);
-        this.JokeBody = JSON.parse(result["_body"]);
+    this.JokePage++;
 
+    this.restapiService.runHttpPost(this.apiUrl+this.JokePage,req)
+      .then((result) => {
+        if (this.JokePageLen === 0){
+          this.JokeBody = JSON.parse(result["_body"]).records;
+        }
+        else {
+          for(var i=0;i<JSON.parse(result["_body"]).records.length;i++) {
+            //console.log("++++++++++"+i);
+            this.JokeBody.push(JSON.parse(result["_body"]).records[i]);
+          }
+        }
+        //console.log("-----------"+this.JokeBody);
+        this.JokePageLen = this.JokeBody.length;
+        //console.log("-----------++"+this.JokePageLen);
+        if(this.JokeNum == 0) {
+          this.JokeNumId = this.JokeBody[this.JokeNum]["id"];
+          this.JokeNumName = this.JokeBody[this.JokeNum]["name"];
+        }
       }, (err) => {
         console.log(err);
       });
